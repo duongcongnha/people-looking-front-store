bf428ba8c0124ea0d909ff416e8681d3588a7985
Nha Duong <duongcongnhahs@gmail.com>
2022-01-20 11:35:05 +0700

    add features

--- application/main/app_API.py
+++ application/main/app_API.py
@@ -16,6 +16,21 @@ async def sum(a, b):
     return {f"sum of {a} and {b}": (int(a) + int(b))}
 
 
+@app.post("/num-persons-in-interval")
+async def num_persons_in_interval(start_datetime: str = Form(default=(datetime.now() - timedelta(days=1)).strftime("%d-%m-%Y %H:%M:%S")),
+                        end_datetime: str = Form(default=datetime.now().strftime("%d-%m-%Y %H:%M:%S"))):
+
+    # "12/11/2018 09:15:32"
+    FMT = "%d-%m-%Y %H:%M:%S"
+
+    start_datetime = datetime.strptime(start_datetime, FMT)
+    end_datetime = datetime.strptime(end_datetime, FMT)
+    
+    all_persons = get_persons_in_timerange(start_datetime, end_datetime)
+    
+    return {"num_persons": len(all_persons)}
+
+
 @app.post("/get-persons-in-interval")
 async def get_persons_in_interval(start_datetime: str = Form(default=(datetime.now() - timedelta(days=1)).strftime("%d-%m-%Y %H:%M:%S")),
                         end_datetime: str = Form(default=datetime.now().strftime("%d-%m-%Y %H:%M:%S"))):
@@ -30,9 +45,9 @@ async def get_persons_in_interval(start_datetime: str = Form(default=(datetime.n
     
     return {"all_persons": all_persons}
 
-@app.post("/login/")
-async def login(username: str = Form(default="nhap username do"), password: str = Form(...)):
-    return {"username": username}
+# @app.post("/login/")
+# async def login(username: str = Form(default="nhap username do"), password: str = Form(...)):
+#     return {"username": username}
 
 
 # uvicorn app_API:app --host 0.0.0.0 --port 8000 --reload
